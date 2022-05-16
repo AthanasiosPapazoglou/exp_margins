ffc901bd4f84dbf93a1168d01094771492fda5e3
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2022-05-16 19:07:05 +0300

    initialisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App());
+  runApp(ExpenseMargins());
 }
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class ExpenseMargins extends StatelessWidget {
+  const ExpenseMargins({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Expense Margins',
-          style: TextStyle(
-            fontSize: 16
-          ),
-        )
+        leading: const Icon(Icons.arrow_back),
       ),
+      body: Mai,
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:exp_margins/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(const ExpenseMargins());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
