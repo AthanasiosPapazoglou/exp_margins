67bbe8f6f7b11a8051b94ad33fb5091fea416951
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2022-05-16 18:58:59 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/exp_margins/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exp_margins
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Expense Margins',
+          style: TextStyle(
+            fontSize: 16
+          ),
+        )
+      ),
+    );
+  }
+}
