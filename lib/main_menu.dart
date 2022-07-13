7d775186b3a68adf3e837063367b93d2e651b347
AthanasiosPapazoglou <athanasiospap95@gmail.com>
2022-07-13 21:19:26 +0300

    updated scrolling and bottom navigation bar behaivior

--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -1,64 +1,71 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'dart:ffi';
+
+import 'package:exp_margins/navPages/flows_page.dart';
 import 'package:flutter/material.dart';
 
 import 'navPages/margins_page.dart';
+import 'navPages/profile_page.dart';
 
 class MainMenu extends StatefulWidget {
-  const MainMenu({Key key}) : super(key: key);
+  const MainMenu({Key key, this.initialPage}) : super(key: key);
+
+  final String initialPage;
 
   @override
   State<MainMenu> createState() => _MainMenuState();
 }
 
 class _MainMenuState extends State<MainMenu> {
-  int _selectedIndex = 0;
-  PageController pageController = PageController();
+  int _selectedNavigationIndex = 0;
+  String _currentPage = 'MarginsPage';
+  //PageController pageController = PageController();
 
-  void onNavBarTap(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-    pageController.jumpToPage(index);
+  @override
+  void initState() {
+    super.initState();
+    _currentPage = widget.initialPage ?? _currentPage;
   }
 
   @override
   Widget build(BuildContext context) {
+    final tabs = {
+      'MarginsPage': MarginsPage(),
+      'FlowsPage': FlowsPage(),
+      'ProfilePage': ProfilePage(),
+    };
     return Scaffold(
-      body: PageView(
-        controller: pageController,
-        children: <Widget>[
-          MarginsPage(),
-          Center(
-            child: Text(
-              'Flows',
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-          Center(
-            child: Text(
-              'Edit',
-              style: TextStyle(color: Colors.black),
-            ),
-          ),
-        ],
-        onPageChanged: onNavBarTap,
-      ),
+      body: tabs[_currentPage],
+      //extendBody: true,
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.margin,
-              ),
-              label: 'Margins'),
-          BottomNavigationBarItem(icon: Icon(Icons.swap_horiz), label: 'Flows'),
-          BottomNavigationBarItem(icon: Icon(Icons.edit), label: 'Edit'),
-        ],
-        currentIndex: _selectedIndex,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        selectedItemColor: Theme.of(context).primaryColor,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blueGrey.shade900,
+        selectedItemColor: Colors.green.shade400,
         unselectedItemColor: Colors.white,
-        onTap: onNavBarTap,
+        currentIndex: _selectedNavigationIndex,
+        onTap: (i) {
+          setState(() {
+            _currentPage = tabs.keys.toList()[i];
+            _selectedNavigationIndex = i;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt_outlined),
+            label: 'Margins',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.swap_horiz,
+            ),
+            label: 'Flows',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
